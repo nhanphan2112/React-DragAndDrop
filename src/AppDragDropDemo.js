90b356878aa1cdc30078214006e58bc538557ff1
Nhan Phan <nphanacc@gmail.com>
2021-09-20 11:05:22 -0700

    create wip and complete arrays in render and display them to the correct location

--- src/AppDragDropDemo.js
+++ src/AppDragDropDemo.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 class AppDragDropDemo extends Component {
 	state = {
-		task: [
+		tasks: [
 			{ name: "Angular", category: "wip", bgcolor: "yellow" },
 			{ name: "React", category: "wip", bgcolor: "pink" },
 			{ name: "Vue", category: "complete", bgcolor: "skyblue" },
@@ -11,7 +11,38 @@ class AppDragDropDemo extends Component {
 	};
 
 	render() {
-		return <div className="container-drag">Drag and Drop Demo</div>;
+    var tasks = {
+      wip: [],
+      complete: []
+    }
+
+    this.state.tasks.forEach((t) =>{
+        tasks[t.category].push(
+          <div key={t.name}
+               className="draggable"
+               style={{backgroundColor: t.bgcolor}}
+          >
+            {t.name}
+          </div>
+        );
+    });
+
+
+		return (
+    <div className="container-drag">
+      <h2 className="header">Drag and Drop Demo</h2>
+
+      <div className="wip">
+        <span className="task-header">WIP</span>
+        {tasks.wip}
+      </div>
+
+      <div className="droppable">
+        <span className="task-header">Complete</span>
+        {tasks.complete}
+      </div>
+    </div>
+    );
 	}
 }
 
