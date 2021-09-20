26522dc4fbc830e46e23b8e63a813314acf85ff3
Nhan Phan <nphanacc@gmail.com>
2021-09-20 10:48:36 -0700

    Initial Setup and CSS style

---
+++ src/AppDragDropDemo.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './App.css';
+
+
+class AppDragDropDemo extends Component{
+  render(){
+    return(<div className="container-drag">Drag and Drop Demo</div>);
+  }
+}
+
+export default AppDragDropDemo;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppDragDropDemo from './AppDragDropDemo';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <App />,
+    <AppDragDropDemo />,
   document.getElementById('root')
 );
 
